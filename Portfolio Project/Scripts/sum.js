51f96f9493751c432ba3a3966f4887ea46973de3
austinpearson12 <52326121+austinpearson12@users.noreply.github.com>
2019-07-21 11:30:05 -0400

    updates

--- Portfolio Project/Scripts/sum.js
+++ Portfolio Project/Scripts/sum.js
@@ -1,29 +1,42 @@
-﻿$("btnSum4").click(function () {
-    //Set an empty Array
-    var sumArray = new Array();
+﻿$(function () {
 
-    //Get the Input Numbers
-    sumArray.push(+$("$sumNum12").val());
-    sumArray.push(+$("$sumNum21").val());
-    sumArray.push(+$("$sumNum31").val());
-    sumArray.push(+$("$sumNum41").val());
-    sumArray.push(+$("$sumNum51").val());
-    sumArray.push(+$("$sumNum61").val());
 
-    var Kvalue = +$("#Kvalue").val();
 
-    for (index = 0, found = false; index < sumArray.length - 1; index++) {
-        var position = sumArray.indexOf(kvalue - sumArray[index], index + 1);
-        if (position > 0) {
-            found = true;
-            $("#factOut4").text("Congrats! K has been found!");
-            $("#mathEquation").text(index + "+" + sumArray + "=" + Kvalue);
-            break;
+    $("btnSum4").click(function () {
+        //Set an empty Array
+        var sumArray = new Array();
+
+        //Get the Input Numbers
+        sumArray.push(+$("$sumNum12").val());
+        sumArray.push(+$("$sumNum21").val());
+        sumArray.push(+$("$sumNum31").val());
+        sumArray.push(+$("$sumNum41").val());
+        sumArray.push(+$("$sumNum51").val());
+        sumArray.push(+$("$sumNum61").val());
+
+        var Kvalue = +$("#KValue").val();
+
+        for (index = 0, found = false; index < sumArray.length - 1; index++) {
+            var position = sumArray.indexOf(kvalue - sumArray[index], index + 1);
+            if (position > 0) {
+                found = true;
+                $("#factOut4").text("Congrats! K has been found!");
+                $("#mathEquation").text(index + "+" + sumArray + "=" + Kvalue);
+                break;
+            }
         }
-    }
-    if (found) {
-        $("#factOut4").text("K is not in this Array.");
-    }
+        if (found) {
+            $("#factOut4").text("K is not in this Array.");
+        }
+        $("#clearBtn5").click(function () {
+            $("#factOut4").text("");
+            $("#sumNum12").text("");
+            $("#sumNum21").text("");
+            $("#sumNum31").text("");
+            $("#sumNum41").text("");
+            $("#sumNum51").text("");
+            $("#sumNum61").text("");
+        });
 
-
+    });
 });
